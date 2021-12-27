9fa88321b8c16d22c9ef7fb78a1d048fb613cf82
singhalaashi <36998451+singhalaashi@users.noreply.github.com>
2021-12-28 00:26:01 +0530

    added/updated/deleted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,5 +4,8 @@ public class LoginPage {
 
 	public static void main(String arr[]) {
 		System.out.println("Hello home page");
+		String username = "admin";
+		String password = "admin";
+		System.out.println("****Logged in****");
 	}
 }
--- src/tests/HomePageTest.java
+++
@@ -1,8 +0,0 @@
-package tests;
-
-public class HomePageTest {
-
-	public void test2() {
-		System.out.println("home test 1");
-	}
-}
